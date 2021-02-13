2310d7adaa52730aa4c0df787ce319956875c1ff
Furkan KURUTAS <fkurutass@gmail.com>
2021-02-13 16:53:25 +0300

    creating log folder and file

--- configuration.go
+++ configuration.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
@@ -21,6 +23,8 @@ type Configuration struct {
 	} `yaml:"providers"`
 }
 
+const logPathDirectory = "logs"
+
 // Initialize parse configuration file content into configuration struct
 func (c *Configuration) Initialize() {
 	config, configError := ioutil.ReadFile("configuration.yaml")
@@ -34,5 +38,22 @@ func (c *Configuration) Initialize() {
 
 	if err != nil {
 		log.Fatalf("Error occured: %v", err)
+	}
+}
+
+// CreateLogging is check directory then create log folder and file
+func (c *Configuration) CreateLogging() {
+	if _, err := os.Stat(logPathDirectory); os.IsNotExist(err) {
+		fmt.Println("Log directory not exists")
+		os.Mkdir(logPathDirectory, os.ModePerm)
+
+		file, fileErr := os.OpenFile(
+			filepath.Join(logPathDirectory, "logs.txt"), os.O_RDONLY|os.O_CREATE, 0644)
+
+		if fileErr != nil {
+			log.Fatalf("Error occrured at creating log file: %v", fileErr)
+		}
+
+		file.Close()
 	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
-type Job struct 
-
 func main() {
 	var config Configuration
 	config.Initialize()
+	config.CreateLogging()
 }
